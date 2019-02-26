5a0280fbb5f16b2d1ce7d7d75561cf27fcd4935c
gavindent <gavindent@hotmail.com>
2019-02-25 23:17:15 -0800

    Tree script added with ability to be watered and take water away, leaves change colors depending on if the tree is watered or not

---
+++ HopefullyNotBoring/Assets/_Scripts/TreeManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeManager : MonoBehaviour
+{
+    private bool hasWater;
+    private Material branch1, branch2, branch3;
+
+    public Color greenBranch;
+    public Color dryBranch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasWater = false;
+        branch1 = this.transform.GetChild(1).transform.GetComponent<Renderer>().material;
+        branch2 = this.transform.GetChild(2).transform.GetComponent<Renderer>().material;
+        branch3 = this.transform.GetChild(3).transform.GetComponent<Renderer>().material;
+        UpdateColors();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void giveWater()
+    {
+        hasWater = true;
+        UpdateColors();
+    }
+
+    public void depleteWater()
+    {
+        hasWater = false;
+        UpdateColors();
+    }
+
+    private void UpdateColors()
+    {
+        Color newColor;
+        if (hasWater)
+            newColor = greenBranch;
+        else
+            newColor = dryBranch;
+        branch1.color = newColor;
+        branch2.color = newColor;
+        branch3.color = newColor;
+    }
+}
