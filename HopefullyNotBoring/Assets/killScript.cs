49890b8b47a3acc89480e58876bd165f28c6e083
M3RLN <32886755+M3RLN@users.noreply.github.com>
2019-02-26 18:12:31 -0800

    Added Spikes

---
+++ HopefullyNotBoring/Assets/killScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killScript : MonoBehaviour
+{
+    public GameObject SceneManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        SceneManager.GetComponent<SceneScript>().ResetScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
