5745473c5b1b3eebc3b865663917638034e04085
gavindent <gavindent@hotmail.com>
2019-03-03 18:11:47 -0800

    Pause prefab added, press esc key to pause game. Must assign PauseGameManager obj to Canvas Panel for it to work

---
+++ HopefullyNotBoring/Assets/_Scripts/PauseController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseController : MonoBehaviour
+{
+    public bool isPaused;
+    public GameObject panel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause()
+    {
+        if (Time.timeScale == 0f)
+        {
+            Time.timeScale = 1f;
+            isPaused = false;
+            if (panel == null)
+            {
+                panel = GameObject.Find("Panel");
+            }
+            panel.SetActive(false);
+        }
+        else
+        {
+            Time.timeScale = 0f;
+            isPaused = true;
+            panel.SetActive(true);
+        }
+    }
+
+    public void QuitGame()
+    {
+        UnityEngine.Application.Quit();
+    }
+}
