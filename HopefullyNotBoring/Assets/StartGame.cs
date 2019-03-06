824ca8a628b4e896e4cc4ca8385079511598d558
M3RLN <32886755+M3RLN@users.noreply.github.com>
2019-03-05 16:53:34 -0800

    Intro Screen

---
+++ HopefullyNotBoring/Assets/StartGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public GameObject m_buttonText;
+    // Start is called before the first frame update
+    void Awake()
+    {
+    }
+
+    public void GoToLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
