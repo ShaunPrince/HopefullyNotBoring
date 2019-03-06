3920ca0fd217454791ad5c0ef9dad7f9731a4fe3
M3RLN <32886755+M3RLN@users.noreply.github.com>
2019-03-05 17:59:33 -0800

    Adding tutorial screen and fixing the level

---
+++ HopefullyNotBoring/Assets/SwitchScreens.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchScreens : MonoBehaviour
+{
+
+    public GameObject m_nextScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SwitchTheScreen()
+    {
+        m_nextScreen.SetActive(true);
+        gameObject.transform.parent.gameObject.SetActive(false);
+    }
+}
