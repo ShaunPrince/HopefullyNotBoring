7f1f1f4469f6e4738db8660c727caf4a65061b1c
Unknown <DJGunn@uci.edu>
2019-03-12 16:33:06 -0700

    Created Audio Manager, added prefab to title screen
    Created an Audio Manager singleton called AudioManager and made a prefab of it. I've put an instance of the prefab in the title screen. It doesn't destroy on load, so it should persist through scene changes.
    
    Its functionality can be called in two ways:
    AudioManager.playMusic([AudioClip Here]);
    AudioManager.playSFX([AudioClip Here]);

---
+++ HopefullyNotBoring/Assets/AudioManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance = null;
+
+    [HideInInspector]
+    public AudioSource musicSource;
+    [HideInInspector]
+    public AudioSource SFXSource;
+
+    private void Awake() //called when our object wakes up
+    {
+        if (instance == null) instance = this;
+        else if (instance != this) Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);        
+    }
+
+    public void playMusic(AudioClip music) //can be called by other scripts to play music
+    {
+        musicSource.clip = music;
+
+        musicSource.Play();
+    }
+
+    public void playSFX(AudioClip SFX) //can be called by other scripts to play SFX
+    {
+        SFXSource.clip = SFX;
+
+        SFXSource.Play();
+    }
+}
